1e7ed5445c73255aa15c7c90ef81f299aa844cd1
Matias YB <matias.ybaeza@gmail.com>
2025-05-26 19:27:54 -0400

    modificando HTML

---
+++ index.js
@@ -0,0 +1,5 @@
+let title = document.querySelector(".title")
+let subtitle = document.querySelector(".title_mini")
+let boton = document.querySelector(".button")
+
+boton.addEventListener("click", =>(call back))
