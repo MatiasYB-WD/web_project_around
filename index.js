2eb52995bd538c60d6455c34b5f923d0585edcf2
Matias YB <matias.ybaeza@gmail.com>
2025-06-02 15:45:34 -0400

    se agrega page, se linkea paginas css

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ let title = document.querySelector(".title")
 let subtitle = document.querySelector(".title_mini")
 let boton = document.querySelector(".button")
 
-boton.addEventListener("click", =>(call back))
