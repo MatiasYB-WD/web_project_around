60654c8eb6550977a889f7b16282af5a41473f40
Matias YB <matias.ybaeza@gmail.com>
2025-06-14 20:46:53 -0400

    se agrega fonts inter

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let title = document.querySelector(".title")
+let title = document.querySelector(".header__title")
 let subtitle = document.querySelector(".subtitle")
 let line = document.querySelector(".line")
 
