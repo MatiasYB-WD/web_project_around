e982f93807e823689c8f8f7ef688af161365cb2e
Matias YB <matias.ybaeza@gmail.com>
2025-06-16 12:49:02 -0400

    se agrega button y elemento popup

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let title = document.querySelector(".header__title")
-let subtitle = document.querySelector(".subtitle")
-let line = document.querySelector(".line")
+//comentarios en js
+console.log("hola mundo")
 
+const button= document.querySelector(".profile__button");
+const popupNode= document.querySelector(".popup");
+const popupCloseButton= document.querySelector(".popup__close")
+
+button.addEventListener("click", function(event){
+    popupNode.classList.add(".popup__show");
+}
+)
+
+popupCloseButton.addEventListener("click", function(){
+    popupNode.classList.remove(".popup__close");
+})
