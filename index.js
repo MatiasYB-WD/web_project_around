d404f1c9507ec5533773f719cfb58f3778916da9
Matias YB <matias.ybaeza@gmail.com>
2025-06-26 23:08:05 -0400

    se realizan terminaciones en css y html para ancho de 320px

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //comentarios en js
-console.log("hola mundo")
 
 const button= document.querySelector(".profile__button");
 const popupNode= document.querySelector(".popup");
 const popupCloseButton= document.querySelector(".popup__close")
 
-button.addEventListener("click", function(event){
-    popupNode.classList.add(".popup__show");
+button.addEventListener("click", function(){
+    popupNode.classList.add("popup__show");
 }
 )
 
 popupCloseButton.addEventListener("click", function(){
-    popupNode.classList.remove(".popup__close");
-})
+    popupNode.classList.remove("popup__show");
+}
+)
